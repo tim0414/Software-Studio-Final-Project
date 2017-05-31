6097a403486fa372bcc0e242b829e5f8a6856c61
TimChen <tim04142013@gmail.com>
2017-06-01 07:08:59 +0800

    E10 done
    created misssiles

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour {
 
 	private Transform target;
 	public float speed = 70f;
+	public float explosionRadius = 0f;
 
 	public GameObject impactEffect;
 
@@ -29,14 +30,40 @@ public class Bullet : MonoBehaviour {
 		}
 
 		transform.Translate(dir.normalized*distanceThisFrame, Space.World);
+		transform.LookAt(target);
 		
 	}
 
 	void HitTarget(){
 		GameObject effectsIns = (GameObject) Instantiate(impactEffect, transform.position, transform.rotation);
-		Destroy(effectsIns, 2f);
-		Destroy(target.gameObject);
+		Destroy(effectsIns, 5f);
 
+		if(explosionRadius>0f){
+			Explode();
+		}else{
+			Damage(target);
+		}
 		Destroy(gameObject);
+	}
+
+	void Explode(){
+		Collider[] colliders =  Physics.OverlapSphere(transform.position, explosionRadius);
+		foreach (Collider collider in colliders){
+			if(collider.tag == "Enemy"){
+				Damage(collider.transform);
+			}
+		}
+	}
+
+	void Damage (Transform enemy){
+		Destroy(enemy.gameObject);
+	}
+
+
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, explosionRadius);	
 	}
 }
